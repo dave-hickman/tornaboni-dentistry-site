01d16c82a969149fa81e8994cc9174d1731673e1
dave-hickman <dave.robert.hickman@gmail.com>
2023-06-28 16:15:38 +0100

    add map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,36 @@
-import "../styling/Map.css"
+import "../styling/Map.css";
+import GoogleMapReact from "google-map-react";
+import dotenv from 'dotenv'
+dotenv.config()
 
 interface Location {
-    address: string;
-    lat: number;
-    lng: number;
+  address: string;
+  lat: number;
+  lng: number;
 }
 
 interface Props {
-    location: Location,
-    zoomLevel: number
+  location: Location;
+  zoomLevel: number;
 }
 
-const MapPage = ({location, zoomLevel}: Props) => {
+const MapPage = ({ location, zoomLevel }: Props) => {
+    const apiKey = process.env.API_KEY || ""
+  return (
+    <section className="map-container">
+      <div className="google-map">
+        <GoogleMapReact 
+        bootstrapURLKeys={{key: apiKey}}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}>
+        {/* <LocationPin
+        lat={location.lat}
+        lng={location.lng}
+        text={location.address}/> */}
+        </GoogleMapReact>
+      </div>
+    </section>
+  );
+};
 
-    return( <section className="map-container"><p>This is the map</p></section>)
-}
-
-export default MapPage
+export default MapPage;
